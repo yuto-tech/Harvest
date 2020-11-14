d85bfd6610b76742f4eadc9e6a642d00c54d4fc3
片岡優斗 <kataokayuuto@kataokayuutonoMacBook-puro.local>
2020-11-15 02:38:44 +0900

    HEROKU_POSTGRESQL_RED_URL

--- models/sequelize-loader.js
+++ models/sequelize-loader.js
@@ -1,7 +1,7 @@
 'use strict';
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize(
-  process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost/web_app',
+  process.env.HEROKU_POSTGRESQL_RED_URL || 'postgres://postgres:postgres@localhost/web_app',
   {
     operatorsAliases: false
   });
